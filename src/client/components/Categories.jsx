25dbc7f71cdf31f90c4e59e76a1674e1dad7c446
Wayne Wilcox <wilcox_wayne@hotmail.com>
2020-08-12 19:25:58 -0400

    implemented correct fetch request for Categories.jsx

--- src/client/components/Categories.jsx
+++ src/client/components/Categories.jsx
@@ -1,56 +1,64 @@
-import React, { Component, useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { Component, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/categories.scss';
 import styled from 'styled-components';
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // 2 buttons with Links on them
 function Categories() {
-    const [state, setState] = useState([]);
-    const [hasError , setHasError] = useState(false);
+  const [state, setState] = useState([]);
+  const [hasError, setHasError] = useState(false);
 
-    useEffect(() => {
-        fetch('/api/categories')
-            .then(response => {
-              console.log('response ' , response);
-              return response.json()})
-                .then(data => { 
-                  console.log('data' , data);
-                for(let i = 0; i < data.length; i++){
-                  console.log('data.name', data[i].name)
-                  console.log('data.id' , data[i].id);
-                  //console.log('data.categories' , data.categories);
-                } 
-                  
-                    state = setState(data);
-                    console.log(state);
-                  //let {name , id} = state; 
-                  })
-                    .catch(err => setHasError(true))
-    }, [state])
+  useEffect(() => {
+    fetch('/api/categories')
+      .then((response) => {
+        console.log('response ', response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log('data', data);
+        for (let i = 0; i < data.length; i++) {
+          console.log('data.name', data[i].name);
+          console.log('data.id', data[i].id);
+        }
+
+        setState(data);
+        console.log('state', state);
+        // let {name , id} = state;
+      })
+      .catch((err) => {
+        console.log('error', err);
+        setHasError(true);
+      });
+  }, []);
 
   return (
     <div className="mainContainer">
       <h1>Categories!!!</h1>
       <div>
-      { hasError? <div> Oh, no! There was an error.</div> 
-        :(state.map( (category) => {
-          console.log('category' , category);
-             <li className="category">
-                    <Link to={{
-                        pathname: `/categories/:category.${category.id}`,
-                        state: {category}
-                    }}/>
-                    {category.name}
-             </li>
-
-        }))
-      }
+        {hasError ? (
+          <div> Oh, no! There was an error.</div>
+        ) : (
+          state.map((category) => {
+            return (
+              <li className="category" key={category.id}>
+                <Link
+                  to={{
+                    pathname: `/categories/${category.id}`,
+                    state: { category },
+                  }}
+                >
+                  {category.name}
+                </Link>
+              </li>
+            );
+          })
+        )}
       </div>
-      <button>
+      <button type="button">
         <Link to="/categories/new">Define new category</Link>
       </button>
-      <button>
+      <button type="button">
         <Link to="/technologies/new">Define new technology</Link>
       </button>
     </div>
@@ -58,36 +66,36 @@ function Categories() {
 }
 export default Categories;
 
-/* 
+/*
 const renderCategories = () => {
   return state.map((category) => {
-    const {name , id } = category; 
+    const { name, id } = category;
 
     return (
       <li className="category">
-      <Link to={{
-          pathname: `/categories/:category.${id}` , 
-          state: {category}
-        }}/>
+        <Link
+          to={{
+            pathname: `/categories/:category.${id}`,
+            state: { category },
+          }}
+        />
         Hi, I'm an li and I work now, yay!!!
         {name}
       </li>
-    )}
-  )}
+    );
+  });
+};
 
-        return (
-          <div className= "main-container">
-            <h1>Categories</h1>
-            <div className="categories">
-            {renderCategories()}
-            </div>
-            <button>
-              <Link to="/categories/new">Define new category</Link>
-            </button>
-            <button>
-              <Link to="/technologies/new">Define new technology</Link>
-            </button>
-          </div>
-        )
-
+return (
+  <div className="main-container">
+    <h1>Categories</h1>
+    <div className="categories">{renderCategories()}</div>
+    <button>
+      <Link to="/categories/new">Define new category</Link>
+    </button>
+    <button>
+      <Link to="/technologies/new">Define new technology</Link>
+    </button>
+  </div>
+);
 */
